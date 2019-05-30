69107f4a9756baf22a093f4c81c901acfdaa3550
harithj <harithjaved@gmail.com>
2019-05-30 07:18:47 +0300

    configure HAProxy image via packer

--- packer/haproxy/setup.sh
+++ packer/haproxy/setup.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
-setupEnvrionment () {
-  printf '=========================== Setup the environment ======================== \n'
+# The below function installs HAProxy
+installHaProxy () {
+  printf '=========================== Install HAProxy ======================== \n'
 
   sudo apt-get update
   sudo apt-get install -y haproxy
+}
+
+# The below function copies the HAProxy's config file to the appropriate location
+# and it restarts the HAProxy service
+configureHaProxy () {
+  printf '=========================== Configure HAProxy ======================== \n'
+
   sudo rm -rf /etc/haproxy/haproxy.cfg
   sudo cp /tmp/configs/haproxy.cfg /etc/haproxy/haproxy.cfg
   sudo systemctl restart haproxy
 }
 
-run () {
-  setupEnvrionment
+# main function invokes all the other functions
+main () {
+  installHaProxy
+  configureHaProxy
 }
 
-run
+main
