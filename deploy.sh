b49c483f3f4bfaf55e93334a14fc8e2c90c98df5
harithj <harithjaved@gmail.com>
2019-05-30 07:26:38 +0300

    fine tune deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
+# build the database master image
 buildMasterImage() {
   touch master_image_output.log
-  packer build packer/master/packer.json 2>&1 | tee master_image_output.log
+  packer build packer/db-master/packer.json 2>&1 | tee master_image_output.log
   MASTER_IMG_TAG="$(grep 'A disk image was created:' master_image_output.log | cut -d' ' -f8)"
   echo $MASTER_IMG_TAG
 }
 
-buildSlaveImage() {
+# build the database slave images
+buildSlaveImages() {
   touch slave1_image_output.log
-  packer build packer/slaves/packer.json 2>&1 | tee slave1_image_output.log
+  packer build packer/db-slave1/packer.json 2>&1 | tee slave1_image_output.log
+  SLAVE1_IMG_TAG="$(grep 'A disk image was created:' slave1_image_output.log | cut -d' ' -f8)"
+  echo $SLAVE1_IMG_TAG
+
+  touch slave1_image_output.log
+  packer build packer/db-slave2/packer.json 2>&1 | tee slave1_image_output.log
   SLAVE1_IMG_TAG="$(grep 'A disk image was created:' slave1_image_output.log | cut -d' ' -f8)"
   echo $SLAVE1_IMG_TAG
 }
 
+# build the etcd image
+buildEtcdImage() {
+  touch haproxy_image_output.log
+  packer build packer/ectd/packer.json 2>&1 | tee haproxy_image_output.log
+  HAPROXY_IMG_TAG="$(grep 'A disk image was created:' haproxy_image_output.log | cut -d' ' -f8)"
+  echo $HAPROXY_IMG_TAG
+}
+
+# build the HAProxy image
 buildHaproxyImage() {
   touch haproxy_image_output.log
   packer build packer/haproxy/packer.json 2>&1 | tee haproxy_image_output.log
@@ -21,17 +37,19 @@ buildHaproxyImage() {
   echo $HAPROXY_IMG_TAG
 }
 
+# create the infrastructre
 deployImages() {
   cd terraform
   terraform init
   terraform apply
 }
 
-run () {
+main () {
   buildMasterImage
-  buildSlaveImage
+  buildSlaveImages
+  buildEtcdImage
   buildHaproxyImage
   deployImages
 }
 
-run
+main
